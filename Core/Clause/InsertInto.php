1bcf3c718359dbd231c8165d63e60c71a0a800ed
Dominik Klapuch <klapuchdominik@gmail.com>
2019-09-01 00:21:33 +0200

    WIP

--- Core/Clause/InsertInto.php
+++ Core/Clause/InsertInto.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 namespace Klapuch\Sql\Clause;
 
 use Klapuch\Sql\Expression\Expression;
-use Klapuch\Sql\Expression\NamedParameters;
+use Klapuch\Sql;
 
 final class InsertInto implements Clause {
 	/** @var string */
@@ -13,7 +13,11 @@ final class InsertInto implements Clause {
 	/** @var mixed[] */
 	private $values;
 
+	/** @var Sql\PreparedStatement */
+	private $preparedStatement;
+
 	public function __construct(string $table, array $values) {
+		$this->preparedStatement = new Sql\PreparedStatement($values);
 		$this->table = $table;
 		$this->values = $values;
 	}
@@ -23,26 +27,11 @@ final class InsertInto implements Clause {
 			'INSERT INTO %s (%s) VALUES (%s)',
 			$this->table,
 			implode(', ', array_keys($this->values)),
-			implode(', ', array_map(static function (string $column, $value): string {
-				if ($value instanceof Expression) {
-					return (new NamedParameters($value, $column))->sql();
-				}
-				return sprintf(':%s', $column);
-			}, array_keys($this->values), $this->values)),
+			implode(', ', $this->preparedStatement->sql()),
 		);
 	}
 
 	public function parameters(): array {
-		return array_reduce(
-			array_keys($this->values),
-			function (array $parameters, string $column): array {
-				$value = $this->values[$column];
-				if ($value instanceof Expression) {
-					return $parameters += (new NamedParameters($value, $column))->parameters();
-				}
-				return $parameters += [$column => $value];
-			},
-			[],
-		);
+		return $this->preparedStatement->parameters();
 	}
 }
